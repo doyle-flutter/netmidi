7a37c90f9375577e9a91123e7b9cb64fac5a20bc
doyle-flutter <56661529+doyle-flutter@users.noreply.github.com>
2020-04-20 14:08:35 +0900

    midisocketserver
    midisocketserver

---
+++ midisocketserver.js
@@ -0,0 +1,19 @@
+// socket.io
+// node.js(express)
+
+var app = require('express')(); 
+var http = require('http').Server(app); 
+var io = require('socket.io')(http); 
+
+app.get('/', function(req, res){ 
+    res.send("hi"); 
+}); 
+
+io.on('connection', function(socket){ 
+    socket.on('send_message', function(msg){ 
+        io.emit('receive_message', msg); }
+    ); 
+});
+
+
+http.listen(3000, function(){ console.log('listening on *:3000'); });
